4929b1fb9198445e6a6e5470d0734bd8bcea43d4
Jeff Baziuk <jeff.baziuk@oracle.com>
2021-03-08 14:10:34 -0700

    Updated tests.

--- src/LeetCode/Easy/ClimbingStairs.java
+++ src/LeetCode/Easy/ClimbingStairs.java
@@ -18,4 +18,22 @@ public class ClimbingStairs {
             return cache[1];
         }
     }
+
+    public static int OneTwoOrThreeStepsToNthStair(int n) {
+        if(n == 0){
+            return 0;
+        }
+        int[] cache = new int[]{1, 1, 2};
+        for (int i = 3; i <= n; i++) {
+            int currentTotal = cache[0] + cache[1] + cache[2];
+            cache[0] = cache[1];
+            cache[1] = cache[2];
+            cache[2] = currentTotal;
+        }
+        if (n == 1) {
+            return cache[0];
+        } else {
+            return cache[2];
+        }
+    }
 }
--- test/LeetCodeTests/ClimbingStairsTests.java
+++ test/LeetCodeTests/ClimbingStairsTests.java
@@ -7,39 +7,45 @@ import org.testng.annotations.Test;
 public class ClimbingStairsTests {
 
     @Test
-    public void ZeroStepsTest() {
+    public void OneOrTwoStepsToZeroStepsTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(0);
         Assert.assertEquals(results, 0);
     }
 
     @Test
-    public void OneStepTest() {
+    public void OneOrTwoStepsToOneStepTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(1);
         Assert.assertEquals(results, 1);
     }
 
     @Test
-    public void fiveStepsTest() {
+    public void OneOrTwoStepsToFiveStepsTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(5);
         Assert.assertEquals(results,8);
     }
 
     @Test
-    public void sixStepsTest() {
+    public void OneOrTwoStepsToSixStepsTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(6);
         Assert.assertEquals(results,13);
     }
 
     @Test
-    public void thirteenStepsTest() {
+    public void OneOrTwoStepsToThirteenStepsTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(13);
         Assert.assertEquals(results,377);
     }
 
     @Test
-    public void fortyFiveStepsTest() {
+    public void OneOrTwoStepsToFortyFiveStepsTest() {
         int results = ClimbingStairs.OneOrTwoStepsToNthStair(45);
         Assert.assertEquals(results,1836311903);
+    }
+
+    @Test
+    public void OneTwoOrThreeStepsTo4StepsTest() {
+        int results = ClimbingStairs.OneTwoOrThreeStepsToNthStair(4);
+        Assert.assertEquals(results,7);
     }
 
 }
