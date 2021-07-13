690865cf2dbd2a211aed55e68fee40536ef05722
Jeff Baziuk <jeff.baziuk@oracle.com>
2021-07-13 13:17:01 -0600

    Cleaning non needed output files.

--- src/LeetCode/Easy/ClimbingStairs.java
+++ src/LeetCode/Easy/ClimbingStairs.java
@@ -36,4 +36,22 @@ public class ClimbingStairs {
             return cache[2];
         }
     }
+
+    public static int stepsToNthStair(int numberOfWays, int stepHeight) {
+        if(stepHeight == 0){
+            return 0;
+        }
+        int[] cache = new int[]{1, 1, 2};
+        for (int i = 3; i <= stepHeight; i++) {
+            int currentTotal = cache[0] + cache[1] + cache[2];
+            cache[0] = cache[1];
+            cache[1] = cache[2];
+            cache[2] = currentTotal;
+        }
+        if (stepHeight == 1) {
+            return cache[0];
+        } else {
+            return cache[2];
+        }
+    }
 }
