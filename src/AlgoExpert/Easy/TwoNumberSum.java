e55c5717c0256e996433825e39f05b8cd1783898
Jeff Baziuk <jeff.baziuk@oracle.com>
2021-09-13 17:56:29 -0600

    Added a method to handle all pairs found.

--- src/AlgoExpert/Easy/TwoNumberSum.java
+++ src/AlgoExpert/Easy/TwoNumberSum.java
@@ -1,5 +1,6 @@
 package AlgoExpert.Easy;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,19 +24,20 @@ public class TwoNumberSum {
         return new int[0];
     }
 
-    public static int[] twoNumberSum2(int[] numbers, int targetSum) {
+    public static ArrayList<int[]> returnAllPairsForTwoNumberSum(int[] numbers, int targetSum) {
         Set<Integer> nums = new HashSet<>();
+        ArrayList<int[]> pairs = new ArrayList<>();
+
         for (int num : numbers) {
             int potentialMatch = targetSum - num;
             if (nums.contains(potentialMatch)) {
-                System.out.printf("%s,%s%n", num, potentialMatch);
-                return new int[]{num, potentialMatch};
+                System.out.printf("We found a pair: %s,%s%n", num, potentialMatch);
+                pairs.add(new int[]{num, potentialMatch});
             } else {
                 nums.add(num);
             }
         }
-        System.out.println("We found no pairs.");
-        return new int[0];
+        return pairs;
     }
 
 
--- test/AlgoExpertTests/Easy/TwoNumberSumTests.java
+++ test/AlgoExpertTests/Easy/TwoNumberSumTests.java
@@ -4,12 +4,13 @@ import AlgoExpert.Easy.TwoNumberSum;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class TwoNumberSumTests {
 
     @Test
     public void defaultPairFound() {
         int[] results = TwoNumberSum.twoNumberSum(TwoNumberSum.array, TwoNumberSum.targetSum);
-        Assert.assertNotNull(results);
         Assert.assertEquals(results[0], -1);
         Assert.assertEquals(results[1], 11);
     }
@@ -20,6 +21,13 @@ public class TwoNumberSumTests {
         int targetSum = 27;
         int[] results = TwoNumberSum.twoNumberSum(array, targetSum);
         Assert.assertEquals(results, new int[0]);
+    }
+
+    @Test
+    public void manyPairsFound() {
+        int[] array = {3, 5, -4, 8, 11, 1, -1, 5, 6, 2, 9, 11};
+        ArrayList<int[]> results = TwoNumberSum.returnAllPairsForTwoNumberSum(array, TwoNumberSum.targetSum);
+        Assert.assertEquals(results.size(), 4);
     }
 
 
