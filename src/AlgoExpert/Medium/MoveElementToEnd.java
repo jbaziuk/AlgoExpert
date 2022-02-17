e69188a39f944355e4ccde0b67d8c28b0d4f1377
Jeff Baziuk <jeff.baziuk@oracle.com>
2022-02-17 13:45:30 -0700

    Updated move element to end alg.

--- src/AlgoExpert/Medium/MoveElementToEnd.java
+++ src/AlgoExpert/Medium/MoveElementToEnd.java
@@ -6,32 +6,30 @@ import java.util.List;
 public class MoveElementToEnd {
 
     public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-        int start = 0;
-        int end = array.size() - 1;
-
-        if (array.equals(null) || array.size() == 0) {
+        if (array == null || array.size() == 0) {
             return new ArrayList<>();
         }
 
+        int start = 0;
+        int end = array.size() - 1;
+
         while (start < end) {
-            if (array.get(start).equals(toMove) && !array.get(end).equals(toMove)) {
-                swap(array, start, end);
-                start += 1;
-                end -= 1;
-            } else if (!array.get(start).equals(toMove)) {
-                start += 1;
-            } else if (array.get(end).equals(toMove)) {
-                end -= 1;
+            while (start < end && array.get(end) == toMove) {
+                end--;
             }
+
+            if (array.get(start) == toMove) {
+                swap(start, end, array);
+            }
+            start++;
         }
         return array;
     }
 
-    public static void swap(List<Integer> array, int start, int end) {
+    public static void swap(int start, int end, List<Integer> array) {
         int temp = array.get(start);
         array.set(start, array.get(end));
         array.set(end, temp);
     }
-
 
 }
