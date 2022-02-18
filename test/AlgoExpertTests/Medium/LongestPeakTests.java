0b68c0dcba7fdc1d56603fd437eb32ccc54f1326
Jeff Baziuk <jeff.baziuk@oracle.com>
2022-02-18 13:27:22 -0700

    Added longest peak alg and tests.

---
+++ test/AlgoExpertTests/Medium/LongestPeakTests.java
@@ -0,0 +1,21 @@
+package AlgoExpertTests.Medium;
+
+import AlgoExpert.Medium.LongestPeak;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LongestPeakTests {
+
+    @Test
+    public void longestPeakExample() {
+        int result = LongestPeak.longestPeak(new int[] {1,2,3,3,4,0,10,6,5,-1,-3,2,3});
+        Assert.assertEquals(result, 6);
+    }
+
+    @Test
+    public void OnePeak() {
+        int result = LongestPeak.longestPeak(new int[] {0,4,2});
+        Assert.assertEquals(result, 3);
+    }
+
+}
