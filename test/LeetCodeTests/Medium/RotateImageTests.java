1f08267d0ca53601b3781477ac8acbe623bb1e11
Jeff Baziuk <jeff.baziuk@oracle.com>
2022-02-27 13:15:45 -0700

    More rotate tests.

--- test/LeetCodeTests/Medium/RotateImageTests.java
+++ test/LeetCodeTests/Medium/RotateImageTests.java
@@ -6,8 +6,35 @@ import org.testng.annotations.Test;
 
 public class RotateImageTests {
 
+
+
     @Test
-    public void RotateTest() {
+    public void rotate2x2ForwardsTest() {
+        int[][] array = {{ 1, 2 },
+                         { 4, 3 }};
+        RotateImage.rotate(array, "forward");
+        Assert.assertEquals(array, array);
+    }
+
+    @Test
+    public void rotate2x2BackwardsTest() {
+        int[][] array = {{ 1, 2 },
+                         { 4, 3 }};
+        RotateImage.rotate(array, "back");
+        Assert.assertEquals(array, array);
+    }
+
+    @Test
+    public void rotate3x3ForwardsTest() {
+        int[][] array = {{1, 2, 3},
+                         {8, 9, 4},
+                         {7, 6, 5}};
+        RotateImage.rotate(array, "forward");
+        Assert.assertEquals(array, array);
+    }
+
+    @Test
+    public void rotate4x4ForwardsTest() {
         int[][] array = {{ 1, 2, 3,4 },
                         { 12,13,14,5 },
                         { 11,16,15,6 },
@@ -17,27 +44,24 @@ public class RotateImageTests {
     }
 
     @Test
-    public void smallRotateForwardTest() {
-        int[][] array = {{ 1, 2 },
-                         { 4, 3 }};
+    public void rotate5x5ForwardsTest() {
+        int[][] array = {{1, 2, 3, 4, 5 },
+                         {16,17,18,19, 6  },
+                         {15,24,25,20, 7 },
+                         {14,23,22,21, 8 },
+                         {13,12,11,10, 9 }};
         RotateImage.rotate(array, "forward");
         Assert.assertEquals(array, array);
     }
 
     @Test
-    public void smallRotateBackwardsTest() {
-        int[][] array = {{ 1, 2 },
-                         { 4, 3 }};
+    public void rotate5x5BackwardsTest() {
+        int[][] array = {{1, 2, 3, 4, 5 },
+                {16,17,18,19, 6  },
+                {15,24,25,20, 7 },
+                {14,23,22,21, 8 },
+                {13,12,11,10, 9 }};
         RotateImage.rotate(array, "back");
-        Assert.assertEquals(array, array);
-    }
-
-    @Test
-    public void threeByThreeTest() {
-        int[][] array = {{1, 2, 3},
-                         {8, 9, 4},
-                         {7, 6, 5}};
-        RotateImage.rotate(array, "forward");
         Assert.assertEquals(array, array);
     }
 
