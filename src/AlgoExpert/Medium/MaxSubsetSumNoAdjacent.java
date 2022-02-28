0223c7c804dd67474356d63c54f4196969c9fde7
Jeff Baziuk <jeff.baziuk@oracle.com>
2022-02-28 11:20:06 -0700

    Added max subset sum dp method.

--- src/AlgoExpert/Medium/MaxSubsetSumNoAdjacent.java
+++ src/AlgoExpert/Medium/MaxSubsetSumNoAdjacent.java
@@ -2,7 +2,21 @@ package AlgoExpert.Medium;
 
 public class MaxSubsetSumNoAdjacent {
 
-
+    public static int maxSubsetSumNoAdjacent(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        } else if (array.length == 1) {
+            return array[0];
+        }
+        int second = array[0];
+        int first = Math.max(array[0], array[1]);
+        for (int i = 2; i < array.length; i++) {
+            int current = Math.max(first, second + array[i]);
+            second = first;
+            first = current;
+        }
+        return first;
+    }
 
     public int rob(int[] nums, int[] memo) {
         return this.robFrom(0, nums, memo);
--- test/AlgoExpertTests/Medium/MaxSubsetSubNoAdjacentTests.java
+++ test/AlgoExpertTests/Medium/MaxSubsetSubNoAdjacentTests.java
@@ -9,13 +9,29 @@ import java.util.Arrays;
 public class MaxSubsetSubNoAdjacentTests {
 
     @Test
-    public void maxSum() {
+    public void maxSumUsingRobMethod() {
         int[] array = {75, 105, 120, 75, 90, 135};
         int[] memo = new int[100];
         Arrays.fill(memo, -1);
         MaxSubsetSumNoAdjacent sum = new MaxSubsetSumNoAdjacent();
         int result = sum.rob(array, memo);
         Assert.assertEquals(result, 330);
+    }
+
+    @Test
+    public void maxSum() {
+        int[] array = {75, 105, 120, 75, 90, 135};
+        MaxSubsetSumNoAdjacent sum = new MaxSubsetSumNoAdjacent();
+        int result = MaxSubsetSumNoAdjacent.maxSubsetSumNoAdjacent(array);
+        Assert.assertEquals(result, 330);
+    }
+
+    @Test
+    public void simpleMaxSum() {
+        int[] array = {12, 10, 5, 7};
+        MaxSubsetSumNoAdjacent sum = new MaxSubsetSumNoAdjacent();
+        int result = MaxSubsetSumNoAdjacent.maxSubsetSumNoAdjacent(array);
+        Assert.assertEquals(result, 19);
     }
 
 }
